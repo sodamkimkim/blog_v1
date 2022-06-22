799e297c1fce0f0228f4fbcdd72aaf488b2f3901
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2022-06-22 13:42:47 +0900

    sql파일 추가

---
+++ files/220622.sql
@@ -0,0 +1,31 @@
+use blog;
+select * from user;
+
+--  create table User (
+--        id integer not null auto_increment,
+--         createDate datetime(6),
+--         email varchar(255),
+--         password varchar(255),
+--         username varchar(255),
+--         primary key (id)
+--     ) engine=InnoDB
+    
+    
+--         create table User (
+--        id integer not null auto_increment,
+--         createDate datetime(6),
+--         email varchar(50) not null,
+--         password varchar(100) not null,
+--         username varchar(30) not null,
+--         primary key (id)
+--     ) engine=InnoDB
+
+select * from user;
+desc user;
+select * from board;
+desc board;
+select * from reply;
+desc reply;
+create database blog3;
+use blog3;
+desc board;
